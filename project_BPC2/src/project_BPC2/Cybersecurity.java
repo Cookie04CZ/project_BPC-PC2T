54303ee802860f06a03c4c3655e4635af90957af
Cookie04CZ <janban04@gmail.com>
2025-03-29 15:35:23 +0100

    predchozi commit, ale se souborama pepla

---
+++ project_BPC2/src/project_BPC2/Cybersecurity.java
@@ -0,0 +1,13 @@
+package project_BPC2;
+
+class Cybersecurity extends Student{
+
+	public Cybersecurity(int ID, String name, String surname, int birth) {
+		super(ID, name, surname, birth);
+		// TODO Auto-generated constructor stub
+	}
+
+	public void generateHash() {
+		System.out.println("Hash brasko");
+	}
+}
--- project_BPC2/src/project_BPC2/Student.java
+++ project_BPC2/src/project_BPC2/Student.java
@@ -5,28 +5,20 @@ public class Student {
     private String name;
     private String surname;
     private int birth;
-    private int branch;
 
-    public Student(int ID, String name, String surname, int birth, int branch) {
+    public Student(int ID, String name, String surname, int birth) {
     	this.ID = ID;
         this.name = name;
         this.surname = surname;
         this.birth = birth;
-        this.branch = branch;
     }
     
-    public String getName()
-	{
+    public String getName() {
 		return name;
 	}
 	
-	public String getSurname()
-	{
+	public String getSurname() {
 		return surname;
 	}
 	
-	public int getBranch()
-	{
-		return branch;
-	}
 }
